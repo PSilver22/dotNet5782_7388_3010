09393a21dc7a142c15f7f3601888a19357010859
PSilver22 <75566318+PSilver22@users.noreply.github.com>
2021-10-07 19:14:45 +0300

    initial code - Welcome functions

---
+++ Targil0/Program7388.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Targil0
+{
+	partial class Program
+	{
+		static void Main(string[] args)
+		{
+			Welcome7388();
+			Welcome3010();
+			Console.ReadKey();
+		}
+
+		static partial void Welcome3010();
+
+		private static void Welcome7388()
+		{
+			// get the user's name
+			Console.Write("Enter your name: ");
+			string name = Console.ReadLine();
+
+			// print welcome message
+			Console.WriteLine($"{name}, welcome to my first console application.");
+		}
+	}
+}
