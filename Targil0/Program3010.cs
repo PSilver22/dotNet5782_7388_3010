367cf956e8b1205b76e0f74777494b663f2eb2ce
Benjy Wiener <info@BenjyWiener.com>
2021-10-07 21:29:33 +0300

    Created Program3010.cs

---
+++ Targil0/Program3010.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace Targil0
+{
+    partial class Program
+    {
+		private static void Welcome3010()
+		{
+            Console.WriteLine("oVkbjhmWuVGbKNjW5ZESJlnVyo0bKdEV");
+		}
+	}
+}
