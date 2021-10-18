f4fc2aadc039536e84c3be85e2f816d0e422d500
PSilver22 <75566318+PSilver22@users.noreply.github.com>
2021-10-18 14:09:10 +0300

    Removed DalObject from DataSource class. Refactored static values in DataSource.Config

--- DAL/DalObject.cs
+++ DAL/DalObject.cs
@@ -1,137 +1,137 @@
-﻿namespace IDAL
+﻿using IDAL.DO;
+
+namespace DalObject
 {
-	namespace DO
-	{
-		public partial class DataSource
-		{
-			/// <summary>
-			/// A class that interacts with and performs operations
-			/// on the data source class
-			/// </summary>
-			public class DalObject
-			{
-                /// <summary>
-                /// Gets item from array with ID equal to key
-                /// </summary>
-                /// <param name="key">Key to search for</param>
-                /// <param name="array">Array to search within</param>
-                /// <returns>element with ID equal to key. default value if key isn't found</returns>
-                private T GetItemByKey<T>(int key, T[] array) where T : IIdentifiable
+    /// <summary>
+    /// A class that interacts with and performs operations
+    /// on the data source class
+    /// </summary>
+    public class DalObject
+    {
+        /// <summary>
+        /// Gets item from array with ID equal to key
+        /// </summary>
+        /// <param name="key">Key to search for</param>
+        /// <param name="array">Array to search within</param>
+        /// <returns>element with ID equal to key. default value if key isn't found</returns>
+        private T GetItemByKey<T>(int key, T[] array, int size) where T : IIdentifiable
+        {
+            // search for element in array with id equal to key
+            for (int index = 0; index < size; ++index)
+            {
+                T element = array[index];
+
+                if (element.Id == key)
                 {
-                    // search for element in array with id equal to key
-                    foreach (T element in array)
-                    {
-                        if (element.Id == key)
-                        {
-                            return element;
-                        }
-                    }
-
-                    // if an element with id is not found, return default
-                    return default(T);
-                }
-
-                /// <summary>
-                /// gets a station by id from the stations array
-                /// </summary>
-                /// <param name="id">id of the station</param>
-                /// <returns>Station with the given id if found. null otherwise</returns>
-                public Station GetStation(int id)
-                {
-                    return GetItemByKey<Station>(id, stations);
-                }
-
-                /// <summary>
-                /// gets a drone by id from the drones array
-                /// </summary>
-                /// <param name="id">id of the drone</param>
-                /// <returns>Drone with the given id if found. null otherwise</returns>
-                public Drone GetDrone(int id)
-                {
-                    return GetItemByKey<Drone>(id, drones);
-                }
-
-                /// <summary>
-                /// gets a customer by id from the customers array
-                /// </summary>
-                /// <param name="id">id of the customer</param>
-                /// <returns>Customer with the given id if found. null otherwise</returns>
-                public Customer GetCustomer(int id)
-                {
-                    return GetItemByKey<Customer>(id, customers);
-                }
-
-                /// <summary>
-                /// gets a package by id from the packages array
-                /// </summary>
-                /// <param name="id">id of the package</param>
-                /// <returns>Package with the given id if found. null otherwise</returns>
-                public Package GetPackage(int id)
-                {
-                    return GetItemByKey<Package>(id, packages);
-                }
-
-                /// <summary>
-                /// Creates a string that lists the elements of array
-                /// </summary>
-                /// <param name="array">Array to create list from</param>
-                /// <returns>String that lists the elements of array</returns>
-                private string ListArrayItems<T>(T[] array) where T : struct
-                {
-                    // concatenate every element in array into a list
-                    string result = "";
-                    foreach (T element in array)
-                    {
-                        result += element.ToString() + "\n";
-                    }
-
-                    return result;
-                }
-
-                /// <summary>
-                /// Creates a string with the information for every station in the stations list
-                /// </summary>
-                /// <returns>
-                /// string with the information for every station
-                /// </returns>
-                public string GetStationList() 
-                {
-                    return ListArrayItems<Station>(stations);
-                }
-
-                /// <summary>
-                /// Creates a string with the information for every drone in the drones list
-                /// </summary>
-                /// <returns>
-                /// string with the information for every drone
-                /// </returns>
-                public string GetDroneList()
-                {
-                    return ListArrayItems<Drone>(drones);
-                }
-
-                /// <summary>
-                /// Creates a string with the information for every package in the packages list
-                /// </summary>
-                /// <returns>
-                /// string with the information for every package
-                /// </returns>
-                public string GetPackageList()
-                {
-                    return ListArrayItems<Package>(packages);
-                }
-
-                /// <summary>
-                /// Creates a string with the information for every customer in the customers list
-                /// </summary>
-                /// <returns>
-                /// string with the information for every customer
-                /// </returns>
-                public string GetCustomerList()
-                {
-                    return ListArrayItems<Customer>(customers);
+                    return element;
                 }
             }
-		}
+
+            // if an element with id is not found, return default
+            return default;
+        }
+
+        /// <summary>
+        /// gets a station by id from the stations array
+        /// </summary>
+        /// <param name="id">id of the station</param>
+        /// <returns>Station with the given id if found. null otherwise</returns>
+        public Station GetStation(int id)
+        {
+            return GetItemByKey<Station>(id, DataSource.stations, DataSource.Config.CurrentStationsSize);
+        }
+
+        /// <summary>
+        /// gets a drone by id from the drones array
+        /// </summary>
+        /// <param name="id">id of the drone</param>
+        /// <returns>Drone with the given id if found. null otherwise</returns>
+        public Drone GetDrone(int id)
+        {
+            return GetItemByKey<Drone>(id, DataSource.drones, DataSource.Config.CurrentDronesSize);
+        }
+
+        /// <summary>
+        /// gets a customer by id from the customers array
+        /// </summary>
+        /// <param name="id">id of the customer</param>
+        /// <returns>Customer with the given id if found. null otherwise</returns>
+        public Customer GetCustomer(int id)
+        {
+            return GetItemByKey<Customer>(id, DataSource.customers, DataSource.Config.CurrentCustomersSize);
+        }
+
+        /// <summary>
+        /// gets a package by id from the packages array
+        /// </summary>
+        /// <param name="id">id of the package</param>
+        /// <returns>Package with the given id if found. null otherwise</returns>
+        public Package GetPackage(int id)
+        {
+            return GetItemByKey<Package>(id, DataSource.packages, DataSource.Config.CurrentPackagesSize);
+        }
+
+        /// <summary>
+        /// Creates a string that lists the elements of array
+        /// </summary>
+        /// <param name="array">Array to create list from</param>
+        /// <returns>String that lists the elements of array</returns>
+        private string ListArrayItems<T>(T[] array, int size) where T : struct
+        {
+            // concatenate every element in array into a list
+            string result = "";
+            for (int index = 0; index < size; ++index)
+            {
+                T element = array[index];
+
+                result += element.ToString() + "\n";
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// Creates a string with the information for every station in the stations list
+        /// </summary>
+        /// <returns>
+        /// string with the information for every station
+        /// </returns>
+        public string GetStationList()
+        {
+            return ListArrayItems<Station>(DataSource.stations, DataSource.Config.CurrentStationsSize);
+        }
+
+        /// <summary>
+        /// Creates a string with the information for every drone in the drones list
+        /// </summary>
+        /// <returns>
+        /// string with the information for every drone
+        /// </returns>
+        public string GetDroneList()
+        {
+            return ListArrayItems<Drone>(DataSource.drones, DataSource.Config.CurrentCustomersSize);
+        }
+
+        /// <summary>
+        /// Creates a string with the information for every package in the packages list
+        /// </summary>
+        /// <returns>
+        /// string with the information for every package
+        /// </returns>
+        public string GetPackageList()
+        {
+            return ListArrayItems<Package>(DataSource.packages, DataSource.Config.CurrentPackagesSize);
+        }
+
+        /// <summary>
+        /// Creates a string with the information for every customer in the customers list
+        /// </summary>
+        /// <returns>
+        /// string with the information for every customer
+        /// </returns>
+        public string GetCustomerList()
+        {
+            return ListArrayItems<Customer>(DataSource.customers, DataSource.Config.CurrentCustomersSize);
+        }
 	}
 }
--- DAL/DataSource.cs
+++ DAL/DataSource.cs
@@ -1,35 +1,33 @@
 ﻿using System;
+using IDAL.DO;
 
-namespace IDAL
+namespace DalObject
 {
-    namespace DO
+    /// <summary>
+    /// Class containing information about all drones, stations, customers, and packages
+    /// </summary>
+    public class DataSource
     {
-        /// <summary>
-        /// Class containing information about all drones, stations, customers, and packages
-        /// </summary>
-        public partial class DataSource
+        const int MaxDrones = 10;
+        const int MaxStations = 5;
+        const int MaxCustomers = 100;
+        const int MaxPackages = 1000;
+
+        internal static Drone[] drones = new Drone[MaxDrones];
+        internal static Station[] stations = new Station[MaxStations];
+        internal static Customer[] customers = new Customer[MaxCustomers];
+        internal static Package[] packages = new Package[MaxPackages];
+
+        public static void Initialize() { }
+
+        internal class Config
         {
-            const int MaxDrones = 10;
-            const int MaxStations = 5;
-            const int MaxCustomers = 100;
-            const int MaxPackages = 1000;
+            // variables with the current size of each of DataSource's arrays
+            internal static int CurrentDronesSize = 0;
+            internal static int CurrentStationsSize = 0;
+            internal static int CurrentCustomersSize = 0;
+            internal static int CurrentPackagesSize = 0;
 
-            static Drone[] drones;
-            static Station[] stations;
-            static Customer[] customers;
-            static Package[] packages;
-
-            public static void Initialize() { }
-
-            class Config
-            {
-                // variables with the first open index of each of DataSource's arrays
-                static int OpenDronesIndex = 0;
-                static int OpenStationsIndex = 0;
-                static int OpenCustomersIndex = 0;
-                static int OpenPackagesIndex = 0;
-
-            }
         }
     }
 }
