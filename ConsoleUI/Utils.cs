dd00d94b2feedf1d55989dd3cb2f07d81b42acf8
Benjy Wiener <info@BenjyWiener.com>
2021-10-26 17:36:07 +0300

    Added some console prompt+parse utils

---
+++ ConsoleUI/Utils.cs
@@ -0,0 +1,42 @@
+﻿using System;
+namespace ConsoleUI
+{
+    public class Utils
+    {
+        public static string Prompt(string prompt)
+        {
+            Console.Write(prompt);
+            return Console.ReadLine();
+        }
+
+        public static int PromptInt(string prompt)
+        {
+            int val;
+            while (!int.TryParse(Prompt(prompt), out val))
+            {
+                Console.WriteLine("Invalid input, please try again.");
+            }
+            return val;
+        }
+
+        public static double PromptDouble(string prompt)
+        {
+            double val;
+            while (!double.TryParse(Prompt(prompt), out val))
+            {
+                Console.WriteLine("Invalid input, please try again.");
+            }
+            return val;
+        }
+
+        public static T PromptEnum<T>(string prompt) where T : struct
+        {
+            T val;
+            while (!Enum.TryParse(Prompt(prompt), out val))
+            {
+                Console.WriteLine("Invalid input, please try again.");
+            }
+            return val;
+        }
+    }
+}
