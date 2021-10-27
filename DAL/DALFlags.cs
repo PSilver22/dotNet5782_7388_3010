e121c823a76ce9dd25252349de57f8474ced1649
Benjy Wiener <info@BenjyWiener.com>
2021-10-27 23:03:42 +0300

    Changed values of WeightCategory to start at 0
    Fixes invalid values to randomization in DataSource.initialize

--- DAL/DALFlags.cs
+++ DAL/DALFlags.cs
@@ -8,7 +8,7 @@ namespace IDAL
 {
     namespace DO
     {
-        public enum WeightCategory { heavy = 3, medium = 2, light = 1 };
+        public enum WeightCategory { heavy = 2, medium = 1, light = 0 };
 
         public enum DroneStatus { free, maintenance, delivery };
 
