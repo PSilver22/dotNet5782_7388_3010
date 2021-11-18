629d4f0ac8615f3025ff6ceee5a46a392c2f7200
Benjy Wiener <info@BenjyWiener.com>
2021-11-18 22:03:50 +0200

    Removed unnecessary usings

--- DAL/Exceptions/InvalidIdException.cs
+++ DAL/Exceptions/InvalidIdException.cs
@@ -1,22 +1,18 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace IDAL
 {
-	namespace DO
-	{
-		class InvalidIdException : Exception 
-		{
-			public InvalidIdException() : base() { }
+    namespace DO
+    {
+        class InvalidIdException : Exception
+        {
+            public InvalidIdException() : base() { }
 
-			public InvalidIdException(string message) : base("Invalid ID Exception: " + message) { }
+            public InvalidIdException(string message) : base("Invalid ID Exception: " + message) { }
 
-			public InvalidIdException(string message, Exception innerException) : base("Invalid ID Exception: " + message, innerException) { }
+            public InvalidIdException(string message, Exception innerException) : base("Invalid ID Exception: " + message, innerException) { }
 
-			public InvalidIdException(int id) : this($"{id} is not valid.") { }
-		}
-	}
+            public InvalidIdException(int id) : this($"{id} is not valid.") { }
+        }
+    }
 }
