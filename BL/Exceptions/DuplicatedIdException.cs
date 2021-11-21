ce6a7f703cb2742d5428845e8ea2acedb53fead3
Benjy Wiener <info@BenjyWiener.com>
2021-11-21 14:33:45 +0200

    Added BL DuplicatedIdException

---
+++ BL/Exceptions/DuplicatedIdException.cs
@@ -0,0 +1,11 @@
+﻿#nullable enable
+
+using System;
+
+namespace IBL
+{
+    class DuplicatedIdException : Exception
+    {
+        public DuplicatedIdException(int id, string? entityName) : base($"A(n) {entityName ?? "entity"} with ID {id} already exists") { }
+    }
+}
