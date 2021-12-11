d7bbf727e3d9e6ad50e7ef09effbf05f67502c9d
BenjyWiener <bmw1821@gmail.com>
2021-12-11 23:20:34 +0200

    Created PL shell

---
+++ PL/DroneListWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System.Windows;
+using System.Windows.Input;
+using IBL;
+
+namespace PL
+{
+    /// <summary>
+    /// Interaction logic for DroneListWindow.xaml
+    /// </summary>
+    public partial class DroneListWindow : Window
+    {
+        static IBL.IBL bl = new BL();
+
+        public DroneListWindow()
+        {
+            InitializeComponent();
+            
+            DronesListView.ItemsSource = bl.GetDroneList();
+        }
+
+        private void NewDrone_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            MessageBox.Show("test");
+        }
+
+        private void NewDrone_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = true;
+        }
+    }
+}
