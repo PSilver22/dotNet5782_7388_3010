e09b0afdf1da8004ac8db5395ab7eb39c9615ac9
BenjyWiener <info@benjywiener.com>
2022-02-21 11:47:38 +0200

    Add battery indicator to drone icon

---
+++ PL/Utilities/CompareConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace PL.Utilities
+{
+    public class CompareConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is IComparable v && parameter is IComparable p)
+                return v.CompareTo(p) switch
+                {
+                    < 0 => -1,
+                    > 0 => 1,
+                    _ => 0
+                };
+
+            throw new InvalidOperationException();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new InvalidOperationException();
+        }
+    }
+}
---
+++ PL/Utilities/ScaleConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace PL.Utilities
+{
+    public class ScaleConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is double v && parameter is double k)
+                    return v * k;
+            return 0;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is double v && parameter is double k)
+                return v / k;
+            return 0;
+        }
+    }
+}
